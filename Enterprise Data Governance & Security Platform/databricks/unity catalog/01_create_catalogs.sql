34567c5890872e293c13635cf62f9b04d0b0364b
Akshay Tekam <tekamakshay11@gmail.com>
2026-08-08 18:01:01 +0530

    Create 01_create_catalogs.sql

---
+++ Enterprise Data Governance & Security Platform/databricks/unity catalog/01_create_catalogs.sql
@@ -0,0 +1,7 @@
+CREATE CATALOG IF NOT EXISTS dev_catalog;
+
+CREATE SCHEMA IF NOT EXISTS dev_catalog.bronze;
+
+CREATE SCHEMA IF NOT EXISTS dev_catalog.silver;
+
+CREATE SCHEMA IF NOT EXISTS dev_catalog.gold;
