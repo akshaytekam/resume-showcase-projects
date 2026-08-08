e37057b1d51e7413b8125dde14650f6587e5454c
Akshay Tekam <tekamakshay11@gmail.com>
2026-08-08 18:39:05 +0530

    folder moved

--- Enterprise Data Governance & Security Platform/databricks/unity catalog/01_create_catalogs.sql
+++
@@ -1,7 +0,0 @@
-CREATE CATALOG IF NOT EXISTS dev_catalog;
-
-CREATE SCHEMA IF NOT EXISTS dev_catalog.bronze;
-
-CREATE SCHEMA IF NOT EXISTS dev_catalog.silver;
-
-CREATE SCHEMA IF NOT EXISTS dev_catalog.gold;
