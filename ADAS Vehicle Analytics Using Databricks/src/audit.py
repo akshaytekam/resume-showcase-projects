8627d5c267134c1c4ff0375df84ac0b10eea4a6b
Akshay Tekam <tekamakshay11@gmail.com>
2026-09-09 11:00:08 +0530

    Update audit.py

--- ADAS Vehicle Analytics Using Databricks/src/audit.py
+++ ADAS Vehicle Analytics Using Databricks/src/audit.py
@@ -1,5 +1,10 @@
 # This module will let Bronze, Silver, Gold, batch, and streaming processes write execution information into the same audit table.
 
+# ============================================================
+# audit.py
+# Generic Audit Framework
+# ============================================================
+
 from datetime import datetime
 from pyspark.sql import SparkSession
 
@@ -19,33 +24,27 @@ def write_audit_record(
     status: str,
     error_message: str = None
 ):
-    """
-    Write one pipeline execution record
-    into the generic audit table.
-    """
 
     audit_table = (
         f"adas_catalog.{environment}.pipeline_audit"
     )
 
-    audit_data = [
-        (
-            pipeline_name,
-            environment,
-            layer,
-            table_name,
-            batch_id,
-            start_time,
-            end_time,
-            records_read,
-            records_written,
-            records_rejected,
-            status,
-            error_message
-        )
-    ]
+    audit_data = [(
+        pipeline_name,
+        environment,
+        layer,
+        table_name,
+        batch_id,
+        start_time,
+        end_time,
+        records_read,
+        records_written,
+        records_rejected,
+        status,
+        error_message
+    )]
 
-    audit_columns = [
+    columns = [
         "pipeline_name",
         "environment",
         "layer",
@@ -62,9 +61,13 @@ def write_audit_record(
 
     audit_df = spark.createDataFrame(
         audit_data,
-        audit_columns
+        columns
     )
 
-    audit_df.write.mode("append").saveAsTable(
-        audit_table
+    (
+        audit_df
+        .write
+        .format("delta")
+        .mode("append")
+        .saveAsTable(audit_table)
     )
